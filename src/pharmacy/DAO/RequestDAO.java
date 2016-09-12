52a8a24ed99f6e79c5f138b9fad66520ffbae061
Komron <komron.bakozoda@gmail.com>
2016-09-12 19:40:46 +0300

    Add DAO for admin requests

---
+++ src/pharmacy/DAO/RequestDAO.java
@@ -0,0 +1,60 @@
+package Request.DAO;
+
+import pharmacy.DAO.DAOInterface;
+import pharmacy.Models.Request;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/** requests to Admin */
+public class RequestDAO extends DAOInterface {
+    public RequestDAO() {
+        createConnection();
+    }
+
+    public List<Request> executeSQLQ(String query){
+        List<Request> list = new ArrayList<Request>();
+        Request temp;
+        try {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int netwid = rs.getInt("networkid");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                int pharmacyid = rs.getInt("pharmacyid");
+
+                temp = new Request();
+
+                temp.setId(id);
+                temp.setNetworkId(netwid);
+                temp.setPharmacyId(pharmacyid);
+                temp.setSurname(surname);
+                temp.setName(name);
+                temp.setPassword(password);
+                temp.setUsername(username);
+
+                list.add(temp);
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public List<Request> getAll() {
+        String sql = "SELECT * FROM requeststoadmin";
+        return executeSQLQ(sql);
+    }
+
+    public List<Request> getAllFor(int id){
+        String sql = "SELECT * FROM requeststoadmin WHERE networkid="+id;
+        return executeSQLQ(sql);
+    }
+}
